d6cebe35b6e34b49279527619086af4edd2d211d
dmji <leyshkinanton@gmail.com>
2024-10-10 01:53:02 +0300

    ++

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/dmji/dmji
 
-go 1.20
+go 1.23.1
--- main.go
+++ main.go
@@ -14,10 +14,10 @@ func exists(path string) (bool, error) {
 }
 
 func main() {
-	if ok, _ := exists("www"); !ok {
-		os.Mkdir("www", 0700)
+	if ok, _ := exists("_site"); !ok {
+		os.Mkdir("_site", 0700)
 	}
 
-	os.WriteFile("www/index.html", []byte("Hello from actions!"), 0644)
-	os.WriteFile("www/hello.html", []byte("Hello from actions 2!"), 0644)
+	os.WriteFile("_site/index.html", []byte("Hello from actions!"), 0644)
+	os.WriteFile("_site/hello.html", []byte("Hello from actions 2!"), 0644)
 }
